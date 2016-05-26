77eb68fb1e27754091861dbcc1edb45ca917fbcb
Meet Mangukiya <meet123mangukiya@gmail.com>
2016-05-26 12:43:16 +0530

    Adding dead functions to class send

--- telewrapper.py
+++ telewrapper.py
@@ -1,8 +1,35 @@
 import requests
 
 # default structure
-'''
 class send:
     def message():
-        pass
-'''
+    	pass
+
+    def photo():
+    	pass
+
+    def audio():
+    	pass
+    def document():
+    	pass
+
+    def sticker():
+    	pass
+
+    def video():
+    	pass
+
+    def voice():
+    	pass
+
+    def location():
+    	pass
+
+    def venue():
+    	pass
+
+    def contact():
+    	pass
+
+    def chataction():
+    	pass
